22677a3d0c3ede0002a07e57d86d7b77ee08475e
Mikel Angelo <124595633+MajkelAndzelo@users.noreply.github.com>
2023-02-27 00:00:46 +0100

    Add files via upload

---
+++ stocks.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+st.write('''
+# Simple Stock Price App
+
+
+Shown are the stock closing price and volume of Google
+
+''')
+
+tickerSymbol = st.text_input("Enter ticker symbol: ")
+tickerData = yf.Ticker(str(tickerSymbol))
+start = st.text_input("Enter start date(yyyy-mm-dd): ")
+end = st.text_input("Enter end date(yyyy-mm-dd or 'today'): ")
+if end == 'today':
+    end = date.today()
+tickerDf = tickerData.history(period='1d', start=start, end=end)
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
