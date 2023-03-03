40364d42a9994549deb2fbb9a1f940ffc4d7621f
Mikel Angelo <124595633+MajkelAndzelo@users.noreply.github.com>
2023-03-03 22:31:25 +0100

    error handling

--- stocks.py
+++ stocks.py
@@ -5,10 +5,7 @@ from datetime import date
 
 st.write('''
 # Simple Stock Price App
-
-
 Shown are the stock closing price and volume of Google
-
 ''')
 
 tickerSymbol = st.text_input("Enter ticker symbol:  (Like TSLA etc.)")
@@ -17,7 +14,10 @@ start = st.text_input("Enter start date(yyyy-mm-dd): ")
 end = st.text_input("Enter end date(yyyy-mm-dd or 'today'): ")
 if end == 'today':
     end = date.today()
-tickerDf = tickerData.history(period='1d', start=start, end=end)
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+try:
+    tickerDf = tickerData.history(period='1d', start=start, end=end)
+    st.line_chart(tickerDf.Close)
+    st.line_chart(tickerDf.Volume)
+except ValueError:
+    st.error("Error: Invalid input. Please enter valid start and end dates in the format 'yyyy-mm-dd'.")
